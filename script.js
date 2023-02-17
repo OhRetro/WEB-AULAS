be5b0175ac3172e53d8d64a9a000a26370895d5d
NOVOTEC-NAKA <00001101752452sp@aluno.educacao.sp.gov.br>
2023-02-17 10:58:07 -0300

    17.2.23

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function pageTitle(title="") {
+    const body_title = document.getElementById("page_body_title");
     if (title != "") {
         title = "/ "+title;
+        const a_link = document.createElement("a");
+        a_link.href = "../../index.html";
+        a_link.text = "Home";
+        body_title.appendChild(a_link);
+
     }
     document.title = "WEB-AULAS "+title;
+    if (title != "") { 
+        //body_title.textContent += title;
+        return;
+    }
+    body_title.textContent = "WEB-AULAS";
 }
