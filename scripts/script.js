e5488004942f6706070457e69d443411d79fba9c
NOVOTEC-NAKA <00001101752452sp@aluno.educacao.sp.gov.br>
2023-03-01 11:56:11 -0300

    1.3.23

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-function pageTitle(title="", prevType=0) {
-    const prevTypes = [["../../index.html", "Home"], ["../index.html", "Voltar"]]
+function pageTitle(title="", prevType=0, typeOne="index.html") {
+    const prevTypes = [["index.html", "Home"], [typeOne, "Voltar"]]
 
     const bodyTitle = document.getElementById("page-body-title");
     bodyTitle.textContent = "";
@@ -29,7 +29,7 @@ function generatePagesLink(section, links) {
         li.className = "zoom zNormal"
 
         const a = document.createElement("a");
-        a.href = "pages/" + element[0] + "/index.html";
+        a.href = element[0] + ".html";
         a.text = element[1];
         
         li.appendChild(a);
