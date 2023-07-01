e156336b784105b566a800db1ba678fdab6820c3
OhRetro <gabrielnacazone@hotmail.com>
2023-07-01 18:03:38 -0300

    1.7.23

---
+++ scripts/animaisN.js
@@ -0,0 +1,64 @@
+let animalUrlBase = "https://naka-hub.vercel.app/animals"
+var animalsJson = {}
+fetch(`${animalUrlBase}/list.json`)
+	.then(response => response.json())
+	.then(response => animalsJson.list = response)
+
+fetch(`${animalUrlBase}/lang.json`)
+	.then(response => response.json())
+	.then(response => animalsJson.lang = response)
+
+function changeImage(imageSrc = "images/black.png", text = " ", loadingMode = false) {
+	const imageDisplay = document.getElementById("image-display")
+	const caption = document.getElementById("caption")
+
+	imageDisplay.src = imageSrc
+	imageDisplay.onload = function () {
+		caption.innerHTML = text
+		if (loadingMode) {
+            imageDisplay.onclick = ""
+        } else {
+            imageDisplay.onclick = function () {
+				open(imageSrc)
+			}
+        }
+	}
+}
+
+async function requestAnimalJSON() {
+	return await requestURL(animalsJsonUrl)
+}
+
+async function getRandomAnimalFile(animal, displayName) {
+	changeImage("images/loading.gif", "Carregando...", true)
+
+	const animalsJson = await requestAnimalJSON()
+	const animals = await animalsJson["media"]
+	const selectedAnimal = chooseRandomItem(animals[animal])
+
+	changeImage(await requestURL(selectedAnimal[0], selectedAnimal[1]), displayName)
+	
+	if (document.getElementById("image-display").src.endsWith(".mp4")) {
+		getRandomAnimalFile(animal, displayName)
+	}
+}
+
+async function generateAnimalOptions(language) {
+	const animalOptions = document.getElementById("animal-options")
+	const animalsJson = await requestAnimalJSON()
+	const animals = await animalsJson["media"]
+	const translations = await animalsJson["translations"][language]
+	const blacklistedAnimals = ["duck"]
+	
+	Object.keys(animals).forEach(function (key) {
+		if (blacklistedAnimals.includes(key)) {
+			return
+		}
+		const formattedKey = formatText(translations[key])
+		generateOption("animal-options", formattedKey, key);
+	})
+
+	document.getElementById("get-animal-button").addEventListener("click", function () {
+		getRandomAnimalFile(animalOptions.value, formatText(translations[animalOptions.value]))
+	}, false);
+}
