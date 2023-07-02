caa00eefcbdb26415f7692daeee528e4e6b31234
OhRetro <gabrielnacazone@hotmail.com>
2023-07-02 04:55:38 -0300

    2.7.23

--- scripts/animaisN.js
+++ scripts/animaisN.js
@@ -1,64 +1,67 @@
-const blacklistedAnimals = ["duck"]
+const blacklistedAnimals = ["duck"];
 
-const animalRequestList = ["list", "lang"]
-const animalUrlBase = "https://naka-hub.vercel.app/animals"
-var animalsJson = {}
+async function requestAnimalsJSON() {
+	const animalRequestList = ["list", "lang"];
+	const animalUrlBase = "https://naka-hub.vercel.app/animals";
+	var animalsJson = {};
 
-animalRequestList.forEach(animalRequest => {
-	fetch(`${animalUrlBase}/${animalRequest}.json`)
-		.then(response => response.json())
-		.then(response => animalsJson[animalRequest] = response)
-})
+	const fetchPromises = animalRequestList.map(animalRequest =>
+		fetch(`${animalUrlBase}/${animalRequest}.json`)
+			.then(response => response.json())
+			.then(response => (animalsJson[animalRequest] = response))
+	);
+
+	await Promise.all(fetchPromises);
+
+	return animalsJson;
+}
 
 function changeImage(imageSrc = "images/black.png", text = " ", loadingMode = false) {
-	const imageDisplay = document.getElementById("image-display")
-	const caption = document.getElementById("caption")
+	const imageDisplay = document.getElementById("image-display");
+	const caption = document.getElementById("caption");
 
-	imageDisplay.src = imageSrc
+	imageDisplay.src = imageSrc;
 	imageDisplay.onload = function () {
-		caption.innerHTML = text
-		if (loadingMode) {
-            imageDisplay.onclick = ""
-        } else {
-            imageDisplay.onclick = function () {
-				open(imageSrc)
-			}
-        }
-	}
+		caption.innerHTML = text;
+	};
 }
 
-function getRandomAnimalFile(animal, displayName) {
-	changeImage("images/loading.gif", "Carregando...", true)
+async function getRandomAnimalFile(animal, displayName) {
+	const animalsJson = await requestAnimalsJSON()
+	
+	changeImage("images/loading.gif", "Carregando...", true);
 
-	const selectedAnimal = chooseRandomItem(animalsJson.list[animal])
+	const selectedAnimal = chooseRandomItem(animalsJson.list[animal]);
 	fetch(selectedAnimal[0])
-		.then(response => response.json())
-		.then(response => {
-			selectedAnimal[1].forEach(pathKey => {
-				response = response[pathKey]
+		.then((response) => response.json())
+		.then((response) => {
+			selectedAnimal[1].forEach((pathKey) => {
+				response = response[pathKey];
 			});
 
-			changeImage(response, displayName)
-		})
+			changeImage(response, displayName);
+		});
 
 	if (document.getElementById("image-display").src.endsWith(".mp4")) {
-		getRandomAnimalFile(animal, displayName)
+		getRandomAnimalFile(animal, displayName);
 	}
 }
 
-function generateAnimalOptions(language) {
-	const animalOptions = document.getElementById("animal-options")
-	const lang = animalsJson.lang[language]
+async function generateAnimalOptions(language) {
+	const animalOptions = document.getElementById("animal-options");
+	const animalsJson = await requestAnimalsJSON()
 	
-	Object.keys(animalsJson.list).forEach(animalKey => {
+	const lang = animalsJson.lang[language];
+
+	Object.keys(animalsJson.list).forEach((animalKey) => {
 		if (blacklistedAnimals.includes(animalKey)) {
-			return
+			return;
 		}
 
 		generateOption("animal-options", lang[animalKey], animalKey);
-	})
+	});
 
 	document.getElementById("get-animal-button").addEventListener("click", () => {
-		getRandomAnimalFile(animalOptions.value, lang[animalOptions.value])
+		getRandomAnimalFile(animalOptions.value, lang[animalOptions.value]);
 	}, false);
 }
